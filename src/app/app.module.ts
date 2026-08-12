0c273ed30cc05b4ceb625af2d49171f91e593364
PABLO ALVES OLIVEIRA <pabloalves1213@gmail.com>
2026-08-11 21:42:46 -0300

    iniciando estrutura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './custom-layout/navbar/navbar.component';
+import { FooterComponent } from './custom-layout/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { ContatoComponent } from './features/contato/contato.component';
+import { SobreMimComponent } from './features/sobre-mim/sobre-mim.component';
+import { HabilidadesComponent } from './features/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ContatoComponent,
+    SobreMimComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
