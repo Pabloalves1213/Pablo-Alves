8050073cf7bfbba5570d5175dc5de4020377cb1e
PABLO ALVES OLIVEIRA <pabloalves1213@gmail.com>
2026-08-31 21:01:18 -0300

    finalizado a estrutura do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { HabilidadesComponent } from './features/habilidades/habilidades.component';
+import { ContatoComponent } from './features/contato/contato.component';
+import { SobreMimComponent } from './features/sobre-mim/sobre-mim.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'sobre-mim', component: SobreMimComponent },
+  { path: 'habilidades', component: HabilidadesComponent },
+  { path: 'contato', component: ContatoComponent }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
